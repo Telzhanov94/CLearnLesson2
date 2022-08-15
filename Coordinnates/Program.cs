ae2a148dbd5aa543a4440868fe1466c991896fb9
Temirlan94 <Telzhanov94@gmail.com>
2022-08-15 16:42:28 +0600

    координаты

---
+++ Coordinnates/Program.cs
@@ -0,0 +1,6 @@
+﻿﻿Console.WriteLine("Введите номер четверти");
+int number = int.Parse (Console.ReadLine ());
+if (number==1) {Console.WriteLine("Рабочая площадь координат (X;Y)");}
+if (number==2) {Console.WriteLine("Рабочая площадь координат (-X;Y)");}
+if (number==3) {Console.WriteLine("Рабочая площадь координат (-X;-Y)");}
+if (number==4) {Console.WriteLine("Рабочая площадь координат (X;-Y)");}
