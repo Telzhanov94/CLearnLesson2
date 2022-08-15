e8964f403fed0eab3121adbc6bd1e0d88b83ab4c
Temirlan94 <Telzhanov94@gmail.com>
2022-08-15 13:04:59 +0600

    Создал Недельку

---
+++ Week2/Week2.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine ("Введите номер дня недели от 1 до 7");
+int number = int.Parse (Console.ReadLine ());
+if (number==1) {Console.WriteLine ("Понедельник он же Monday это будний день");}
+if (number==2) {Console.WriteLine ("Вторник он же Tuesday это будний день");}
+if (number==3) {Console.WriteLine ("Среда он же Wednesday это будний день");}
+if (number==4) {Console.WriteLine ("Черверг он же Thursay это будний день");}
+if (number==5) {Console.WriteLine ("Пятница он же Friday это будний день");}
+if (number==6) {Console.WriteLine ("Суббота он же Saturday это выходной");}
+if (number==7) {Console.WriteLine ("Воскресенье он же Sunday это выходной");}
