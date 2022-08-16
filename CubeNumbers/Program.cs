6c2ad042c97d7c973f0d6f24d47a04bb32d06560
Temirlan94 <Telzhanov94@gmail.com>
2022-08-16 17:58:05 +0600

    redact

--- CubeNumbers/Program.cs
+++ CubeNumbers/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine ("Введите предел до какого числа считать кубы ");
 int numb=int.Parse (Console.ReadLine());
 int count=0;
-while (count<numb)
+while (count<=numb)
 {
     double cube =Math.Pow(count,3);
     Console.Write(cube+" ");
---
+++ SumNumbers/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("Введите предел до какого числа считать сумму от одного ");
+int numb=int.Parse (Console.ReadLine());
+int count=0;
+int sum=0;
+while (count <= numb)
+{
+    sum =sum+count;
+       count++;
+}
+ Console.Write(sum+" ");
