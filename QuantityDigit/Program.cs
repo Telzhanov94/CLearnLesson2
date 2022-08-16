acffa3a9b33695a370d99fe81fd79a8edd83f949
Temirlan94 <Telzhanov94@gmail.com>
2022-08-16 18:45:42 +0600

    Ready

---
+++ QuantityDigit/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter your number, please: ");
+int numb= int.Parse (Console.ReadLine());
+int count =0;
+while (numb != 0)
+    {
+   numb = numb /10;
+    count++;
+    }
+    Console.Write ("In your nubrer "+count+" digit");
