2a8b1bbe398107e6f553bd65e6d98e2740c87166
Temirlan94 <Telzhanov94@gmail.com>
2022-08-16 17:28:44 +0600

    Run

---
+++ CubeNumbers/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine ("Введите предел до какого числа считать кубы ");
+int numb=int.Parse (Console.ReadLine());
+int count=0;
+while (count<numb)
+{
+    double cube =Math.Pow(count,3);
+    Console.Write(cube+" ");
+    count++;
+}
