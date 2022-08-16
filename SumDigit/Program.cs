9b6a4c18c3628126661b7a37b3cf62e6b0a53441
Temirlan94 <Telzhanov94@gmail.com>
2022-08-16 19:13:05 +0600

    Дорабоать надо

---
+++ SumDigit/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your number, please: ");
+string numb= Console.ReadLine();
+int count =0;
+int sum=0;
+while (count != null)
+    {
+    char digitTxt = numb[count];
+    int digit = int.Parse (digitTxt);
+    sum=sum+digit;
+    count++;
+    }
+    Console.Write ("summ digit in your number = "+sum);
