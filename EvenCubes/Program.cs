7e5b3128382eeef22c90434ce84054f69d38627e
Temirlan94 <Telzhanov94@gmail.com>
2022-08-18 13:12:00 +0600

    Ease

---
+++ EvenCubes/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter a limit for calculating multiplication from 1:  ");
+Int64 limit=Int64.Parse (Console.ReadLine());
+int count = 1;
+double multi=1;
+while (count<=limit)
+    {
+    multi=Math.Pow (count,3);
+    if (multi%2==0)
+        {
+        Console.WriteLine (count+" its cube is:"+multi);
+        }
+    count++;
+    }
