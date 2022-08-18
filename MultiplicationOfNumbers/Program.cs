44363853b51b360462c0405c93255804e5b4d121
Temirlan94 <Telzhanov94@gmail.com>
2022-08-18 12:56:03 +0600

    Great

---
+++ MultiplicationOfNumbers/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter a limit for calculating multiplication from 1:");
+int limit=int.Parse (Console.ReadLine());
+int count = 1;
+int multi=1;
+while (count<=limit)
+    {
+    multi=multi*count;
+    count++;
+    }
+Console.WriteLine("Your number = "+multi);
